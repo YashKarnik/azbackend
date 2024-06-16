17d1e7877f5d88d4ae77d09fd043c9664611e3ee
Yash Karnik <yashkarnik2000@gmail.com>
2024-06-16 18:06:18 +0530

    add queue and topics

--- src/main/java/com/azbackend/azbackend/AzbackendApplication.java
+++ src/main/java/com/azbackend/azbackend/AzbackendApplication.java
@@ -1,5 +1,6 @@
 package com.azbackend.azbackend;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzbackendApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(AzbackendApplication.class, args);
 	}
 
